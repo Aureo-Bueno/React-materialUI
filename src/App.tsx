235b17f94e91b2dd195ac10c73818c3a41b079aa
Aureo-Bueno <aureoalexandre@outlook.com>
2022-03-29 10:20:36 -0300

    feat: add react-router-dom, add route /home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
 
 
 export const App = () => {
   return (
-    <div >
-     
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export { };
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+export const AppRoutes = () => { 
+    return (
+        <Routes>
+            <Route path="/home" element={<p>Página Inicial</p>} />
+
+            <Route path="*" element={<Navigate to="/home"/>} />
+
+        </Routes>
+    );
+};
