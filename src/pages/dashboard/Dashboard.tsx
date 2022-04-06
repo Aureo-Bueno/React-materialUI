85cce15e0817c229c47f1ac38c5ff524e0115cf2
Aureo-Bueno <aureoalexandre@outlook.com>
2022-04-06 13:46:07 -0300

    feat: add component ToolBar

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,19 @@
+import { ToolBar } from "../../shared/components";
 import { LayoutBasePage } from "../../shared/layouts";
 
 
 export const Dashboard = () => {
 
     return(
-        <LayoutBasePage title='Página Inicial' toolBar={<>Barra de Ferramentas</>}>
+        <LayoutBasePage 
+          title='Página Inicial' 
+          toolBar={(
+                <ToolBar 
+                   viewInputSearch    
+                   textButtonNew='Nova'
+                />
+            )}
+        >
             Testando
         </LayoutBasePage>
     );
---
+++ src/shared/components/ToolBar/ToolBar.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Icon, Paper, TextField, useTheme } from "@mui/material";
+
+interface IToolBarProps{
+    textSearch?: string;
+    viewInputSearch?: boolean;
+    alterTextSearch?: (newText: string) => void;
+    textButtonNew?: string;
+    viewButtonNew?: boolean;
+    onClickNew?: () => void;
+}
+export const ToolBar: React.FC<IToolBarProps> = ({
+    textSearch =  '',
+    viewInputSearch = false,
+    alterTextSearch,
+    onClickNew,
+    textButtonNew = 'Novo',
+    viewButtonNew = true,
+}) => {
+    const theme = useTheme();
+
+    return (
+        <Box 
+          gap={1}
+          height={theme.spacing(5)} 
+          marginX={1} 
+          padding={1}
+          paddingX={2}
+          display="flex"
+          alignItems="center"
+          component={Paper}
+        >
+            {viewInputSearch && (
+              <TextField
+                    size="small"
+                    value={textSearch}
+                    placeholder='Pesquisar...'
+                    onChange={(e) => alterTextSearch?.(e.target.value)}
+               />
+            )}
+
+            <Box flex={1} display="flex" justifyContent="end">
+               {viewButtonNew && (
+                 <Button
+                   color='primary'
+                   disableElevation
+                   variant='contained'
+                   onClick={onClickNew}
+                   endIcon={<Icon>add</Icon>}
+                 >
+                   {textButtonNew}
+                 </Button>
+               )}
+            </Box>
+        </Box>
+    );
+}
